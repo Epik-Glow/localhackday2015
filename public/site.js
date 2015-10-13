8933eb9d6edd643b9ed92602f21e8b3299731ad3
Brian Liu <b_liu72@yahoo.com>
2015-10-12 21:49:28 -0400

    Added  client html

---
+++ public/site.js
@@ -0,0 +1,42 @@
+
+httpGetAsync("http://45.55.144.205:8080/room", blah);
+
+
+function httpGetAsync(url, callback) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("POST", url, true); // true for asynchronous
+  xmlHttp.send(null);
+}
+
+function blah(e){
+  console.log(e);
+  // 2. This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+
+
+
+
+
+
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'M7lc1UVf-VE',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
