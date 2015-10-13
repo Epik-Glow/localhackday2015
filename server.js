4b05c727a083b5d2df279d832929c5efaf71cbfb
Daven Wu <dtwu@umd.edu>
2015-10-13 00:20:19 -0400

    Testing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require('express')();
-var server = require('http').Server(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var compression = require('compression');
 
