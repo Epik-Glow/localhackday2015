5f31ded1945f98dbfba9224742eeac0e1d3faf33
Daven Wu <dtwu@umd.edu>
2015-10-12 22:44:57 -0400

    Readd server.js

---
+++ server.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var http = require('http').Server(app);
+var app = express();
+var io = require('socket.io')(http);
+var compression = require('compression');
+
+var users = {};
+var numUsers = 0;
+var rooms = {}; // Store rooms using randomly generated 6 dig codes as keys
+
+app.use(compression());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.route('/room')
+    .post(function(req, res) {
+        while (true) {
+            var code = Math.floor(Math.random() * 1000000);
+
+            // Check to see if the room already exists
+            if (!(code.toString() in rooms)) {
+                rooms[code] = [];
+
+                res.status(201).send(JSON.stringify(code));
+                break;
+            }
+        }
+    });
+
+io.on('connection', function(socket) {
+    numUsers++;
+    var user = users[numUsers] = {};
+
+    console.log("User has connected");
+
+    socket.on('roomCode', function(code) {
+        if (code.toString() in rooms) {
+            user.room = code;
+            socket.join(code); // Joins socket with the unique room
+            socket.emit('room', {
+                exists: true
+            });
+        } else {
+            socket.emit('room', {
+                exists: false
+            });
+        }
+    });
+
+    socket.on('playlistAdd', function(videoId) {
+        rooms[user.code].push(videoId);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('User disconnected');
+    });
+});
+
+var server = app.listen(8080, function() {
+    var host = server.address()
+        .address;
+    var port = server.address()
+        .port;
+
+    console.log('Listening on port at http://%s:%s', host, port);
+});
+
